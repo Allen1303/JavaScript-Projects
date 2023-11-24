c2318e3d98c07b8a601054d15ecfd03288002c56
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2023-11-23 19:46:11 -0500

    update JS
    Added toggle feature to Hamburger menu Links

--- JS Website/script.js
+++ JS Website/script.js
@@ -1,122 +1,93 @@
 //Step 1: Create the Main Navigation Container
-/*Create a new nav element using document.createElement('nav').
-body.appendChild(nav).*/
-const nav = document.createElement("nav");
-nav.id = "desktop-nav";
-document.body.appendChild(nav);
+const mainNav = document.createElement("nav");
+mainNav.id = "desktop-nav";
+const logoDiv = document.createElement("div");
+logoDiv.className = "logo";
+logoDiv.textContent = "John Doe";
+mainNav.appendChild(logoDiv);
+document.body.appendChild(mainNav);
 
-//Step 2: Create the Logo Container
-//Create a div element using document.createElement('div').
-const logoElement = document.createElement("div");
-logoElement.className = "logo";
-logoElement.textContent = "John Doe";
-nav.appendChild(logoElement);
+//Step 2: Create the Navigation Links Container
+const navLinksDiv = document.createElement("div");
+mainNav.appendChild(navLinksDiv);
 
-/*Step 3: Create the Navigation Links Container
-Create a second div element using document.createElement('div').*/
-const secondDiv = document.createElement("div");
-nav.appendChild(secondDiv);
+//Step 3: Create the Unordered List (ul) for Navigation Links
+const ulList = document.createElement("ul");
+ulList.className = "nav-links";
+navLinksDiv.appendChild(ulList);
 
-//Step 4: Create the Unordered List (ul) for Navigation Links
-//Create a ul element using document.createElement('ul').
-const ul = document.createElement("ul");
-ul.className = "nav-links";
-secondDiv.appendChild(ul);
-
-//Step 5: Create List Items (li) and Anchor Links (a) using for...of loop
-const linkData = ["About", "Experience", "Projects", "Contact"];
-for (const linkText of linkData) {
+//Step 4: Create List Items (li) and Anchor Links (a) using for...of loop
+const liArray = ["About", "Experience", "Projects", "Contact"];
+for (const liArrayItems of liArray) {
   const liElement = document.createElement("li");
-  const aElement = document.createElement("a");
-  aElement.href = `#${linkText.toLowerCase()}`;
-  aElement.textContent = linkText;
-  liElement.appendChild(aElement);
-  ul.appendChild(liElement);
+  const aTag = document.createElement("a");
+  aTag.href = `#${liArrayItems.toLowerCase()}`;
+  aTag.textContent = liArrayItems;
+  liElement.appendChild(aTag);
+  ulList.appendChild(liElement);
 }
-//Step 6: Append All Elements to the Document
-document.body.appendChild(nav);
+//Step 5: Append All Elements to the Document
+document.body.appendChild(mainNav);
 
-//=====BUILDING THE HAMBURGER NAV=======//
+//Step 6: Building the Hamburger Nav
+// A--Repeate the same process as creating mainNav Element
 const hamburgerNav = document.createElement("nav");
 hamburgerNav.id = "hamburger-nav";
-document.body.appendChild(hamburgerNav);
-
-//STEP 2. Create the Hamburger Logo Element
-// const hamburgerLogo = document.createElement("div");
-//hamburgerLogo.className = "logo";
-//hamburgerLogo.textContent = "John Doe";
-
-//Create the Hamburger Menu Elemnent
-const hamburgerMenu = document.createElement("div");
-hamburgerMenu.className = "hamburger-menu";
-hamburgerNav.appendChild(hamburgerMenu);
-
-// Create the Hamburger Icon Div with Onclick Feature
+const hamburgerLogoDiv = document.createElement("div");
+hamburgerLogoDiv.className = "logo";
+hamburgerLogoDiv.textContent = "John Doe";
+hamburgerNav.appendChild(hamburgerLogoDiv);
+// B-- Create Hamburger div, menu and append it
+const hamburgerMenuDiv = document.createElement("div");
+hamburgerMenuDiv.className = "hamburger-menu";
+hamburgerNav.appendChild(hamburgerMenuDiv);
+// C-- create the hamburger icon,class name and  toggle attribute
 const hamburgerIcon = document.createElement("div");
 hamburgerIcon.className = "hamburger-icon";
-hamburgerIcon.addEventListener("click", toggleMenu);
+hamburgerIcon.onclick = toggleMenu;
+hamburgerMenuDiv.appendChild(hamburgerIcon);
+// D --use the for loop emthod to create the spanElements
 
-//Create and Iterate the Span Elements
 for (let spanIndex = 0; spanIndex < 3; spanIndex++) {
   const spanElement = document.createElement("span");
   hamburgerIcon.appendChild(spanElement);
 }
-hamburgerMenu.appendChild(hamburgerIcon);
 
-//Create Menu Links Container:
-const menuLinks = document.createElement("div");
-menuLinks.className = "menu-links";
+// E-- Repeate step 4 to create menu-links, div and List
+const menuLinksDiv = document.createElement("div");
+menuLinksDiv.className = "menu-links";
 
-//Create the Menu List Items and Anchor tags elemnts
-const menuList = document.createElement("ul");
-// Use the array method for all list items
-const menuItems = ["About", "Experience", "Projects", "Contact"];
-
-//Use a for of Loop for simplier code structure
-for (const listText of menuItems) {
-  //Create both "li" and "a" elements
-  const listItems = document.createElement("li");
-  const anchorTag = document.createElement("a");
-
-  anchorTag.href = `#${listText.toLowerCase()}`;
-  anchorTag.textContent = listText;
-
-  anchorTag.setAttribute(`#${listText.toLowerCase()}`);
-  //Create a Click an Event listener for the Toggle Menu
-  anchorTag.addEventListener("click", toggleMenu);
-
-  //Append the Elements
-  listItems.appendChild(anchorTag);
-  menuList.appendChild(listItems);
+for (const linksElement of liArray) {
+  const menuLi = document.createElement("li");
+  const menuAnchorTag = document.createElement("a");
+  menuAnchorTag.href = `#${linksElement.toLowerCase()}`;
+  menuAnchorTag.textContent = linksElement;
+  menuLi.appendChild(menuAnchorTag);
+  menuLinksDiv.appendChild(menuLi);
 }
-menuLinks.appendChild(menuList);
+hamburgerIcon.appendChild(menuLinksDiv);
+document.body.appendChild(hamburgerNav);
 
+//==Step 7: Create the toggleMenu Function==//
 function toggleMenu() {
-  const toggleList = document.querySelector(".menu-links");
-  toggleList.classList.toggle("visible");
+  // A--Create the menu and Icon Elements
+  const menuLinks = document.querySelector(".menu-links");
+  const menuIcon = document.querySelector(".hamburger-icon");
+  //B--Toggle Classes for the Menu Links and menuIcon:
+  menuLinks.classList.toggle("open");
+  menuIcon.classList.toggle("open");
 }
-// const hamburgerNav = document.getElementById("hamburger-nav");
-//Apend all the Hamburger Icon adn MenuLinks
-hamburgerNav.appendChild(hamburgerIcon);
-hamburgerNav.appendChild(menuLinks);
 
-/*===JAVASCRIPT FUNCTION FOR THE  TOGGLE MENU==*/
-// // function toggleMenuItems() {
-// //   const menu = document.querySelector(".menu-links");
-// //   const icon = document.querySelector(".hamburger-icon");
-// //   menu.classList.toggle("open");
-// //   icon.classList.toggle("open");
-// }
+/*==========CSS  AND SASS STYLING========== */
 
-// /*==========CSS  AND SASS STYLING========== */
-
+// Step 1: Create a Style Element
 const styleElement = document.createElement("style");
-const head = document.getElementsByTagName("head")[0];
+
+// Step 2: Set CSS Styles
 styleElement.innerHTML = `
 /* GENERAL */
 
-  @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Carter+One&family=Ephesis&family=Exo:ital,wght@0,100;0,700;0,800;0,900;1,500;1,700;1,800;1,900&family=Leckerli+One&family=Ma+Shan+Zheng&family=Marck+Script&family=Modak&family=Moirai+One&family=Monoton&family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Pacifico&family=Permanent+Marker&family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,400;1,400&family=Quicksand:wght@300;400;500;600;700&family=Righteous&family=Sacramento&family=Tangerine:wght@700&family=Yuji+Boku&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
 
 * {
   margin: 0;
@@ -124,7 +95,7 @@ styleElement.innerHTML = `
 }
 
 body {
-  font-family: "Montserrat", sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 
 html {
@@ -158,8 +129,7 @@ nav {
 .nav-links {
   gap: 2rem;
   list-style: none;
-  font-size: 1.1rem;
-  font-weight:400;
+  font-size: 1.5rem;
 }
 
 a {
@@ -176,8 +146,7 @@ a:hover {
 }
 
 .logo {
-  font-size: 1.2rem;
-  font-weight:550;
+  font-size: 2rem;
 }
 
 .logo:hover {
@@ -188,6 +157,11 @@ a:hover {
 
 #hamburger-nav {
   display: none;
+}
+@media screen and (max-width: 768px) {
+  #hamburger-nav {
+    display: block; /* Change 'block' to 'flex' or other appropriate value */
+  }
 }
 
 .hamburger-menu {
@@ -224,9 +198,10 @@ a:hover {
 
 .menu-links a {
   display: block;
-  padding: 10px;
+  margin-top:10px;
+  padding-top: 8px;
   text-align: center;
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   color: black;
   text-decoration: none;
   transition: all 0.3 ease-in-out;
@@ -264,7 +239,396 @@ a:hover {
   transform: none;
 }
 
+/* SECTIONS */
 
+section {
+  padding-top: 4vh;
+  height: 96vh;
+  margin: 0 10rem;
+  box-sizing: border-box;
+  min-height: fit-content;
+}
+
+.section-container {
+  display: flex;
+}
+
+/* PROFILE SECTION */
+
+#profile {
+  display: flex;
+  justify-content: center;
+  gap: 5rem;
+  height: 80vh;
+}
+
+.section__pic-container {
+  display: flex;
+  height: 400px;
+  width: 400px;
+  margin: auto 0;
+}
+
+.section__text {
+  align-self: center;
+  text-align: center;
+}
+
+.section__text p {
+  font-weight: 600;
+}
+
+.section__text__p1 {
+  text-align: center;
+}
+
+.section__text__p2 {
+  font-size: 1.75rem;
+  margin-bottom: 1rem;
+}
+
+.title {
+  font-size: 3rem;
+  text-align: center;
+}
+
+#socials-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  gap: 1rem;
+}
+
+/* ICONS */
+
+.icon {
+  cursor: pointer;
+  height: 2rem;
+}
+
+/* BUTTONS */
+
+.btn-container {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+}
+
+.btn {
+  font-weight: 600;
+  transition: all 300ms ease;
+  padding: 1rem;
+  width: 8rem;
+  border-radius: 2rem;
+}
+
+.btn-color-1,
+.btn-color-2 {
+  border: rgb(53, 53, 53) 0.1rem solid;
+}
+
+.btn-color-1:hover,
+.btn-color-2:hover {
+  cursor: pointer;
+}
+
+.btn-color-1,
+.btn-color-2:hover {
+  background: rgb(53, 53, 53);
+  color: white;
+}
+
+.btn-color-1:hover {
+  background: rgb(0, 0, 0);
+}
+
+.btn-color-2 {
+  background: none;
+}
+
+.btn-color-2:hover {
+  border: rgb(255, 255, 255) 0.1rem solid;
+}
+
+.btn-container {
+  gap: 1rem;
+}
+
+/* ABOUT SECTION */
+
+#about {
+  position: relative;
+}
+
+.about-containers {
+  gap: 2rem;
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+}
+
+.about-details-container {
+  justify-content: center;
+  flex-direction: column;
+}
+
+.about-containers,
+.about-details-container {
+  display: flex;
+}
+
+.about-pic {
+  border-radius: 2rem;
+}
+
+.arrow {
+  position: absolute;
+  right: -5rem;
+  bottom: 2.5rem;
+}
+
+.details-container {
+  padding: 1.5rem;
+  flex: 1;
+  background: white;
+  border-radius: 2rem;
+  border: rgb(53, 53, 53) 0.1rem solid;
+  border-color: rgb(163, 163, 163);
+  text-align: center;
+}
+
+.section-container {
+  gap: 4rem;
+  height: 80%;
+}
+
+.section__pic-container {
+  height: 400px;
+  width: 400px;
+  margin: auto 0;
+}
+
+/* EXPERIENCE SECTION */
+
+#experience {
+  position: relative;
+}
+
+.experience-sub-title {
+  color: rgb(85, 85, 85);
+  font-weight: 600;
+  font-size: 1.75rem;
+  margin-bottom: 2rem;
+}
+
+.experience-details-container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.article-container {
+  display: flex;
+  text-align: initial;
+  flex-wrap: wrap;
+  flex-direction: row;
+  gap: 2.5rem;
+  justify-content: space-around;
+}
+
+article {
+  display: flex;
+  width: 10rem;
+  justify-content: space-around;
+  gap: 0.5rem;
+}
+
+article .icon {
+  cursor: default;
+}
+
+/* PROJECTS SECTION */
+
+#projects {
+  position: relative;
+}
+
+.color-container {
+  border-color: rgb(163, 163, 163);
+  background: rgb(250, 250, 250);
+}
+
+.project-img {
+  border-radius: 2rem;
+  width: 90%;
+  height: 90%;
+}
+
+.project-title {
+  margin: 1rem;
+  color: black;
+}
+
+.project-btn {
+  color: black;
+  border-color: rgb(163, 163, 163);
+}
+
+/* CONTACT */
+
+#contact {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  height: 70vh;
+}
+
+.contact-info-upper-container {
+  display: flex;
+  justify-content: center;
+  border-radius: 2rem;
+  border: rgb(53, 53, 53) 0.1rem solid;
+  border-color: rgb(163, 163, 163);
+  background: (250, 250, 250);
+  margin: 2rem auto;
+  padding: 0.5rem;
+}
+
+.contact-info-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  margin: 1rem;
+}
+
+.contact-info-container p {
+  font-size: larger;
+}
+
+.contact-icon {
+  cursor: default;
+}
+
+.email-icon {
+  height: 2.5rem;
+}
+
+/* FOOTER SECTION */
+
+footer {
+  height: 26vh;
+  margin: 0 1rem;
+}
+
+footer p {
+  text-align: center;
+}
+
+/* Sass Code */
+@media screen and (max-width: 1400px) {
+  #profile {
+    height: 83vh;
+    margin-bottom: 6rem;
+  }
+  .about-containers {
+    flex-wrap: wrap;
+  }
+  #contact,
+  #projects {
+    height: fit-content;
+  }
+}
+
+@media screen and (max-width: 1200px) {
+  #desktop-nav {
+    display: none;
+  }
+  #hamburger-nav {
+    display: flex;
+  }
+  #experience,
+  .experience-details-container {
+    margin-top: 2rem;
+  }
+  #profile,
+  .section-container {
+    display: block;
+  }
+  .arrow {
+    display: none;
+  }
+  section,
+  .section-container {
+    height: fit-content;
+  }
+  section {
+    margin: 0 5%;
+  }
+  .section__pic-container {
+    width: 275px;
+    height: 275px;
+    margin: 0 auto 2rem;
+  }
+  .about-containers {
+    margin-top: 0;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  #contact,
+  footer {
+    height: 40vh;
+  }
+  #profile {
+    height: 83vh;
+    margin-bottom: 0;
+  }
+  article {
+    font-size: 1rem;
+  }
+  footer nav {
+    height: fit-content;
+    margin-bottom: 2rem;
+  }
+  .about-containers,
+  .contact-info-upper-container,
+  .btn-container {
+    flex-wrap: wrap;
+  }
+  .contact-info-container {
+    margin: 0;
+  }
+  .contact-info-container p,
+  .nav-links li a {
+    font-size: 1rem;
+  }
+  .experience-sub-title {
+    font-size: 1.25rem;
+  }
+  .logo {
+    font-size: 1.5rem;
+  }
+  .nav-links {
+    flex-direction: column;
+    gap: 0.5rem;
+    text-align: center;
+  }
+  .section__pic-container {
+    width: auto;
+    height: 46vw;
+    justify-content: center;
+  }
+  .section__text__p2 {
+    font-size: 1.25rem;
+  }
+  .title {
+    font-size: 2rem;
+  }
+  .text-container {
+    text-align: justify;
+  }
+}
 `;
-// const head = document.getElementsByTagName("head")[0];
-head.appendChild(styleElement);
+
+// Step 3: Append Style Element to Head
+document.head.appendChild(styleElement);
